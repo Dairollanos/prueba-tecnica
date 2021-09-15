ee9ad0dc27dfa21a9ef93d0ff6dddc785f440a1b
Dairollanos <dairoollanos@gmail.com>
2021-09-15 11:25:22 -0500

    Update models.py

--- pedidos/models.py
+++ pedidos/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save, pre_save
-from django.dispatch import receiver
 
-
-# Create your models here.
 class Articulo(models.Model):
     identificador = models.CharField(max_length=50, primary_key=True)
     nombre = models.CharField(max_length=50)
